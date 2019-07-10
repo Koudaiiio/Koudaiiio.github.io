7756a5c6ce219c93855e45fcd4e083227c97be81
chenyi <251809052@qq.com>
2019-07-10 17:16:55 +0800

    lodash

---
+++ miao/lodash/koudaiiio-lodash.js
@@ -0,0 +1,9 @@
+var koudaiiio = {
+  compact: function(ary) {
+    var result = []
+    for (var i = 0; i < ary.length; i++) {
+      if (ary[i]) result.push(ary[i])
+    }
+    return result
+  }
+}
