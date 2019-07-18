3b469a70fbafdfee98bece45d371f1dea789fdd6
chenyi <251809052@qq.com>
2019-07-18 17:07:10 +0800

    lodash

--- miao/lodash/koudaiiio-lodash.js
+++ miao/lodash/koudaiiio-lodash.js
@@ -1,9 +1,47 @@
-var koudaiiio = {
-  compact: function(ary) {
+var koudaiiio = function () {
+  /**
+   * [chunk description]
+   *
+   * @param   {[type]}  ary   [ary description]
+   * @param   {[type]}  size  [size description]
+   *
+   * @return  {[type]}        [return description]
+   */
+  function chunk(ary, size = 1) {
+    var result = []
+      , i = 0
+    while (i + size <= ary.length) {
+      var a = []
+      for (var j = 0; j < size; j++) {
+        a.push(ary[i++])
+      }
+      result.push(a)
+    }
+    if (i < ary.length) result.push(ary.slice(i))
+    return result
+  }
+
+
+  /**
+   * [compact description]
+   *
+   * @param   {[type]}  ary  [ary description]
+   *
+   * @return  {[type]}       [return description]
+   */
+  function compact(ary) {
     var result = []
     for (var i = 0; i < ary.length; i++) {
       if (ary[i]) result.push(ary[i])
     }
     return result
-  },
-}
+  }
+
+
+
+  
+  return {
+    chunk,
+    compact,
+  }
+} ()
